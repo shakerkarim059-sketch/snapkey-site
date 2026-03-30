0768119148bd71bcdbcfe8be09a929d93206202e
shakerkarim059-sketch <shakerkarim059@gmail.com>
2026-03-30 22:54:17 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "SnapKey",
+  description: "Event Fotos einfach teilen",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
