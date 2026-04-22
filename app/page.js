50778637aac312e0d1f5131b90e550b255a2c535
shakerkarim059-sketch <shakerkarim059@gmail.com>
2026-04-22 16:11:15 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -218,7 +218,7 @@ export default function HomePage() {
       <section style={styles.storySection}>
         <div style={styles.storyImageWrap}>
           <img
-            src="/how-it-works.jpg"
+            src="/hero-nfc.jpg"
             alt="NFC Key neben Smartphone mit geöffnetem Album"
             style={styles.storyImage}
           />
